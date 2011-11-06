609a0855bfd0fd2f0f9e1e900575d6ac8701bb66
mde <mde@fleegix.org>
2011-11-05 22:55:30 -0700

    Revert "Show params with ellipsis in names, display names of optional params in brackets in the method-signature."
    This reverts commit 030b805d27790471e86704870a4ba096c3073f0c.

--- templates/codeview/publish.js
+++ templates/codeview/publish.js
@@ -203,11 +203,13 @@ function makeSignature(params) {
 	if (!params) return "()";
 	var signature = "("
 	+
-	params.map(
+	params.filter(
 		function($) {
-      var name = $.isOptional ?
-          '[' + $.name + ']': $.name;
-			return name;
+			return $.name.indexOf(".") == -1; // don't show config params in signature
+		}
+	).map(
+		function($) {
+			return $.name;
 		}
 	).join(", ")
 	+
